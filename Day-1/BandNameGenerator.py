f1ad8617e91640b1cadd9d8aba5edd259f9b17fc
Simardeep Singh <simardeepsngh1@gmail.com>
2022-02-15 15:33:52 +0530

    Day-1 Challenge

---
+++ Day-1/BandNameGenerator.py
@@ -0,0 +1,12 @@
+# Greeting
+print("Welcome to band name generator.")
+
+# Name of City
+city = input("What is the name of your city that you grew up in?\n")
+
+# Name of Pet
+pet_name = input("What is the name of your pet?\n")
+
+# Generate Band Name
+band_name = city + " " + pet_name
+print("Your Band name could be: ", band_name, end="\n")
